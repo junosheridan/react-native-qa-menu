b05595342f1b1929f17bc20aa9f29c3605d43833
Juno <juno@clearsumm.it>
2023-01-15 21:44:29 +0700

    remove native modules

--- android/src/oldarch/QaMenuViewManagerSpec.kt
+++
@@ -1,9 +0,0 @@
-package com.qamenu
-
-import android.view.View
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.SimpleViewManager
-
-abstract class QaMenuViewManagerSpec<T : View> : SimpleViewManager<T>() {
-  abstract fun setColor(view: T?, value: String?)
-}
